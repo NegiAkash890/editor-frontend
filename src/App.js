8fb0db72ee6782f93016212e53ff4817d6e941fd
Shreya Ghosh <sgshreya024@gmail.com>
2021-12-02 17:29:44 +0530

    Splash Screen Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import Footer from './components/Footer';
-import Main from './components/Main';
-import NavBar from './components/NavBar';
-import './App.css';
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import NavBar from "./components/NavBar";
+import React, { useState, useEffect } from "react";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App"> 
+const App = () => {
+  // Loading state
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Wait for 3 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  return isLoading ? (
+    // If page is still loading then splash screen
+    <div className="splash">
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/main-logo.png`}
+        alt=""
+        width="auto"
+        height="auto"
+      />
+      <PropagateLoader size="20" />
+    </div>
+  ) : (
+    <div className="App">
       {/* Landing Navigation Bar */}
-      
-      <NavBar/>
+
+      <NavBar />
 
       {/* Main Container */}
-      
-      <Main/>
+
+      <Main />
 
       {/* Footer Container */}
 
-      <Footer/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
