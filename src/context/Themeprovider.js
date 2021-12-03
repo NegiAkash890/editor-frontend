4fa7cf0a086c4dd952753955172729de14203f30
Arpan Mondal <arpan19991025@gmail.com>
2021-12-03 12:27:18 +0530

    Create context for theme toggler

---
+++ src/context/Themeprovider.js
@@ -0,0 +1,27 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { ThemeReducer } from "../reducer/ThemeReducer";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useReducer(ThemeReducer, "light");
+
+  useEffect(() => {
+    const currTheme = localStorage.getItem("theme");
+    if (currTheme) {
+      setTheme({ type: "TOGGLE_THEME", payload: { theme: currTheme } });
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  });
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
