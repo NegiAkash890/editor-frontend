7286664237930f337c6861f215cea4e5538ffbc4
Akash Negi <akash8900@gmail.com>
2021-12-06 12:12:19 +0530

    Fixed Conflicts

--- src/components/Splash Screen/SplashScreen.js
+++ src/components/Splash Screen/SplashScreen.js
@@ -1,20 +1,18 @@
-import React from "react";
-import preload from "./SplashScreen.module.css";
-import PropagateLoader from "react-spinners/PropagateLoader";
+import React from 'react';
+import PropagateLoader from 'react-spinners/PropagateLoader';
+import preload from './SplashScreen.module.css';
 
-const Preloader = () => {
-  return (
-    <div className={preload.preload}>
-      <img
-        className={preload.logo}
-        src={`${process.env.PUBLIC_URL}/assets/main-logo.png`}
-        alt=""
-        height="150rem"
-      />
-      <br />
-      <PropagateLoader size="20" />
-    </div>
-  );
-};
+const Preloader = () => (
+  <div className={preload.preload}>
+    <img
+      className={preload.logo}
+      src={`${process.env.PUBLIC_URL}/assets/main-logo.png`}
+      alt=""
+      height="150rem"
+    />
+    <br />
+    <PropagateLoader size="20" />
+  </div>
+);
 
 export default Preloader;
