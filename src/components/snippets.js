adfd8f16ff0b82be8670843075a3a7b843183ab0
Diganta Kr Banik <digantabanik2000@gmail.com>
2021-12-09 16:23:05 +0530

    Improved boilerplate indentation

--- src/components/snippets.js
+++ src/components/snippets.js
@@ -1,21 +1,21 @@
 const snippets = {
-    "cpp" : `#include<bits/stdc++.h>
-    using namespace std;
-    int main(){
-        //Input the code here
-    }`,
-    "java":`import java.util.*;
-    import java.lang.*;
-    import java.io.*;
+  cpp: `#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //Input the code here
+}`,
+  java: `import java.util.*;
+import java.lang.*;
+import java.io.*;
     
-    /* Name of the class has to be "Main" only if the class is public. */
-    class Main
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+    public static void main (String[] args) throws java.lang.Exception
     {
-        public static void main (String[] args) throws java.lang.Exception
-        {
-            // Input the code here
-        }
-    }`,
-    "python":`#Input the code here`,
+        // Input the code here
+    }
+}`,
+  python: '#Input the code here',
 };
 export default snippets;
