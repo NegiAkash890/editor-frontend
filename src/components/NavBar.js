5a0d3c948c70431c8d7e37ddd99c3b45662076ca
Ayush Baranwal <abaran803@gmail.com>
2022-01-13 03:13:16 +0530

    Dark NavBar Added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,34 @@
+/* eslint-disable */
 import React from 'react';
+import { useTheme } from '../context/Providers/Themeprovider';
 import PopupText from './Popup';
 import ThemeToggler from './ThemeToggler/ThemeToggler';
 
-const NavBar = () => (
-  <div className="top__nav">
-    <div className="logo__info">
-      <img
-        src={`${process.env.PUBLIC_URL}/assets/main-logo.png`}
-        alt="site logo"
-        width="50px"
-      />
-      <p id="logo__name">Online Compiler</p>
-    </div>
-    <div className="info__section">
-      <ThemeToggler />
-      <a href="https://github.com/NegiAkash890/editor-backend">
+const NavBar = () => {
+  const { theme } = useTheme();
+  return (
+    <div className="top__nav dark__nav" style={theme === 'dark' ? { backgroundColor: 'black', color: 'white' } : {}}>
+      <div className="logo__info">
         <img
-          src={`${process.env.PUBLIC_URL}/assets/github.png`}
-          title="Go to GitHub Repository"
-          alt="GitHub Icon"
+          src={`${process.env.PUBLIC_URL}/assets/main-logo.png`}
+          alt="site logo"
+          width="50px" style={theme === 'dark' ? { backgroundColor: 'white' } : {}}
         />
-      </a>
-      <PopupText />
+        <p id="logo__name">Online Compiler</p>
+      </div>
+      <div className="info__section">
+        <ThemeToggler />
+        <a href="https://github.com/NegiAkash890/editor-backend">
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/github.png`}
+            title="Go to GitHub Repository"
+            alt="GitHub Icon"
+          />
+        </a>
+        <PopupText />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NavBar;
